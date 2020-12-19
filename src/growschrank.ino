45ba278d3d4ab07e4697a912b2e6a8dfd9f450b6
Elias Kleimeier <elias_kleimeier@outlook.de>
2020-12-20 00:33:48 +0100

    Added partial menu functionality and visuals. Also clock should work now

--- src/growschrank.ino
+++ src/growschrank.ino
@@ -29,29 +29,33 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire);
 // DS3231
 DS3231 Clock;
 RTClib RTC;
-char time_text[6];
 
 // Rotary Encoder
 volatile int counter = 0;
 volatile int currentStateCLK = 0;
 volatile int lastStateCLK = 0;
-volatile int swPressed = 0;
 volatile unsigned long lastSW = 0;
+volatile boolean up = false, down = false;
+volatile boolean swPressed = 0;
 
 // Menu
-String menu_item_0 = "..";	// up
-String menu_item_1 = "Lampe ON/OFF Zeit";
-String menu_item_2 = "Luftpumpe ON/OFF Zeit";
-String menu_item_3 = "Uhrzeit";
-uint8_t menu_item = 0;
-uint8_t last_menu_item = 0;
+String menuItem0 = "..";	// up
+String menuItem1 = "Time";
+String menuItem2 = "Lamp ON/OFF";
+String menuItem3 = "Pump ON/OFF";
+String menuItem4 = "Fan ON/OFF";
+uint8_t menuItem = 0;
+uint8_t subMenuItem = 0;
+uint8_t timeItem = 0;
 uint8_t frame = 0;
 uint8_t page = 0;
-uint8_t lamp_on[2] = {0,0};
-uint8_t lamp_off[2] = {0,0};
-uint8_t pump_on[2] = {0,0};
-uint8_t pump_off[2] = {0,0};
-uint8_t time[2] = {0,0};
+uint8_t time[2] = {0, 0};
+uint8_t lampOn[2] = {0,0};
+uint8_t lampOff[2] = {0,0};
+uint8_t pumpOn[2] = {0,0};
+uint8_t pumpOff[2] = {0,0};
+uint8_t fanOn = 70;
+uint8_t fanOff = 50;
 
 void setup()
 {
@@ -103,21 +107,504 @@ void setup()
 
 void loop()
 {
-	sprintf(time_text, "%02d:%02d", RTC.now().hour(), RTC.now().minute());
+	drawMenu();
+	readEncoder();
+}
+
+void drawMenu()
+{
+	if (page==0)
+		displayMainMenuPage();
+	else if (page==1)
+	{
+		display.setTextSize(1);
+		display.clearDisplay();
+		display.setTextColor(WHITE);
+		display.setCursor(0, 0);
+		display.print("Main Menu");
+		display.drawFastHLine(0, 10, SCREEN_WIDTH, WHITE);
+		
+		if (menuItem==0 && frame==0)
+		{
+			displayMenuItem(menuItem0, 15, true);
+			displayMenuItem(menuItem1, 25, false);
+		}
+		else if (menuItem==1 && frame==0)
+		{
+			displayMenuItem(menuItem0, 15, false);
+			displayMenuItem(menuItem1, 25, true);
+		}
+		else if (menuItem==2 && frame==1)
+		{
+			displayMenuItem(menuItem1, 15, false);	
+			displayMenuItem(menuItem2, 25, true);
+
+		}
+		else if (menuItem==3 && frame==2)
+		{
+			displayMenuItem(menuItem2, 15, false);
+			displayMenuItem(menuItem3, 25, true);
+		}
+		else if (menuItem==4 && frame==3)
+		{
+			displayMenuItem(menuItem3, 15, false);
+			displayMenuItem(menuItem4, 25, true);
+		}
+		display.display();
+	}
+	else if (page==2 && menuItem==1)
+		displayClockMenuPage(menuItem1, time);
+	else if (page==2 && menuItem==2)
+		displayTimerMenuPage(menuItem2, lampOn, lampOff);
+	else if (page==2 && menuItem==3)
+		displayTimerMenuPage(menuItem3, pumpOn, pumpOff);
+	else if (page==2 && menuItem==4)
+		displayOnOffControllerMenuPage(menuItem4, fanOn, fanOff);
+}
+
+void readEncoder()
+{
+	if (up)
+	{
+		up = false;
+		if (page==1)
+		{
+			if (menuItem==2 && frame==1)
+				--frame;
+			else if (menuItem==3 && frame==2)
+				--frame;
+			else if (menuItem==4 && frame==3)
+				--frame;
+			if (menuItem > 0)
+				--menuItem;
+		}
+		else if (page==2)
+		{
+			if (menuItem==1)
+			{
+				if (timeItem==0)
+				{
+					if (time[0]>0)
+						--time[0];
+					else
+						time[0] = 23;
+				}
+				else if (timeItem==1)
+				{
+					if (time[1]>0)
+						--time[1];
+					else
+						time[1] = 59;
+				}
+			}
+			else if (menuItem==2)
+			{
+				if (subMenuItem==0)
+				{
+					if (timeItem==0)
+					{
+						if (lampOn[0]>0)
+							--lampOn[0];
+						else
+							lampOn[0] = 23;
+					}
+					else
+					{
+						if (lampOn[1]>0)
+							--lampOn[1];
+						else
+							lampOn[1] = 59;
+					}
+				}
+				else
+				{
+					if (timeItem==0)
+					{
+						if (lampOff[0]>0)
+							--lampOff[0];
+						else
+							lampOff[0] = 23;
+					}
+					else
+					{
+						if (lampOff[1]>0)
+							--lampOff[1];
+						else
+							lampOff[1] = 59;
+					}
+				}
+			}
+			else if (menuItem==3)
+			{
+				if (subMenuItem==0)
+				{
+					if (timeItem==0)
+					{
+						if (pumpOn[0]>0)
+							--pumpOn[0];
+						else
+							pumpOn[0] = 23;
+					}
+					else
+					{
+						if (pumpOn[1]>0)
+							--pumpOn[1];
+						else
+							pumpOn[1] = 59;
+					}
+				}
+				else
+				{
+					if (timeItem==0)
+					{
+						if (pumpOff[0]>0)
+							--pumpOff[0];
+						else
+							pumpOff[0] = 23;
+					}
+					else
+					{
+						if (pumpOff[1]>0)
+							--pumpOff[1];
+						else
+							pumpOff[1] = 59;
+					}
+				}
+			}
+		}
+
+	}
+	else if (down)
+	{
+		down = false;
+		if (page==1)
+		{
+			if (menuItem==1 && frame==0)
+				++frame;
+			else if (menuItem==2 && frame==1)
+				++frame;
+			else if (menuItem==3 && frame==2)
+				++frame;
+			if (menuItem < 4)
+				++menuItem;
+		}
+		else if (page==2)
+		{
+			if (menuItem==1)
+			{
+				if (timeItem==0)
+				{
+					if (time[0]<23)
+						++time[0];
+					else
+						time[0] = 0;
+				}
+				else if (timeItem==1)
+				{
+					if (time[1]<59)
+						++time[1];
+					else
+						time[0] = 0;
+				}
+			}
+			else if (menuItem==2)
+			{
+				if (subMenuItem==0)
+				{
+					if (timeItem==0)
+					{
+						if (lampOn[0]<23)
+							++lampOn[0];
+						else
+							lampOn[0] = 0;
+					}
+					else
+					{
+						if (lampOn[1]<59)
+							++lampOn[1];
+						else
+							lampOn[1] = 0;
+					}
+				}
+				else
+				{
+					if (timeItem==0)
+					{
+						if (lampOff[0]<23)
+							++lampOff[0];
+						else
+							lampOff[0] = 0;
+					}
+					else
+					{
+						if (lampOff[1]<59)
+							++lampOff[1];
+						else
+							lampOff[1] = 0;
+					}
+				}
+			}
+			else if (menuItem==3)
+			{
+				if (subMenuItem==0)
+				{
+					if (timeItem==0)
+					{
+						if (pumpOn[0]<23)
+							++pumpOn[0];
+						else
+							pumpOn[0] = 0;
+					}
+					else
+					{
+						if (pumpOn[1]<59)
+							++pumpOn[1];
+						else
+							pumpOn[1] = 0;
+					}
+				}
+				else
+				{
+					if (timeItem==0)
+					{
+						if (pumpOff[0]<23)
+							++pumpOff[0];
+						else
+							pumpOff[0] = 0;
+					}
+					else
+					{
+						if (pumpOff[1]<59)
+							++pumpOff[1];
+						else
+							pumpOff[1] = 0;
+					}
+				}
+			}
+		}
+	}
+	else if (swPressed)
+	{
+		swPressed = false;
+		if (page==0)
+			++page;
+		else if (page==1)
+			if (menuItem==0)
+				--page;
+			else
+			{
+				if (menuItem==1)
+				{
+					time[0] = RTC.now().hour();
+					time[1] = RTC.now().minute();
+				}
+				++page;
+			}
+		else if (page==2)
+		{
+			if (menuItem==1)
+			{
+				if (timeItem==0)
+					++timeItem;
+				else
+				{
+					timeItem = 0;
+					page = 1;
+					Clock.setHour(time[0]);
+					Clock.setMinute(time[1]);
+				}
+			}
+			else if (menuItem==2 || menuItem==3)
+			{
+				if (subMenuItem==0)
+				{
+					if (timeItem==0)
+						++timeItem;
+					else if (timeItem==1)
+					{
+						timeItem = 0;
+						++subMenuItem;
+					}
+				}
+				else if (subMenuItem==1)
+				{
+					if (timeItem==0)
+						++timeItem;
+					else if (timeItem==1)
+					{
+						timeItem = 0;
+						subMenuItem = 0;
+						page = 1;
+					}
+				}
+			}
+		}
+	}
+}
+
+void displayMenuItem(String item, uint8_t pos, boolean selected)
+{
+	if (selected)
+		display.setTextColor(BLACK, WHITE);
+	else
+		display.setTextColor(WHITE, BLACK);
+	display.setCursor(0, pos);
+	display.print(">"+item);
+//	display.display();
+}
+
+void displayTimerMenuPage(String menuItem, uint8_t onTime[2], uint8_t offTime[2])
+{
+	display.setTextSize(1);
 	display.clearDisplay();
-  	display.setCursor(0, 0);
-	display.println(time_text);
-	display.println(swPressed);
-  	display.println(counter);
+	display.setTextColor(WHITE);
+	display.setCursor(0, 0);
+	display.print(menuItem); display.drawFastHLine(0, 10, SCREEN_WIDTH, WHITE);
+	display.setCursor(0, 15);
+
+	if (subMenuItem==0)
+	{
+		display.setTextColor(WHITE, BLACK);
+		display.print("On:  ");
+		
+		if (timeItem==0)
+		{
+			display.setTextColor(BLACK, WHITE);
+			display.print(timeToString(onTime[0]));
+			display.setTextColor(WHITE, BLACK);
+			display.print(':');
+			display.println(timeToString(onTime[1]));
+
+		}
+		else if (timeItem==1)
+		{
+			display.print(timeToString(onTime[0]));
+			display.print(':');
+			display.setTextColor(BLACK, WHITE);
+			display.println(timeToString(onTime[1]));
+		}
+		display.setTextColor(WHITE, BLACK);
+		display.print("Off: ");
+		display.print(timeToString(offTime[0]));
+		display.print(':');
+		display.println(timeToString(offTime[1]));
+	}
+	else if (subMenuItem==1)
+	{
+		display.setTextColor(WHITE, BLACK);
+		display.print("On:  ");
+		display.print(timeToString(onTime[0]));
+		display.print(':');
+		display.println(timeToString(onTime[1]));
+		display.print("Off: ");
+
+		if (timeItem==0)
+		{
+			display.setTextColor(BLACK, WHITE);
+			display.print(timeToString(offTime[0]));
+			display.setTextColor(WHITE, BLACK);
+			display.print(':');
+			display.println(timeToString(offTime[1]));
+		}
+		else if (timeItem==1)
+		{
+			display.setTextColor(WHITE, BLACK);
+			display.print(timeToString(offTime[0]));
+			display.print(':');
+			display.setTextColor(BLACK, WHITE);
+			display.println(timeToString(offTime[1]));
+		}
+	}
 	display.display();
-  	delay(100);
+}
+
+void displayClockMenuPage(String menuItem, uint8_t time[2])
+{
+	display.setTextSize(1);
+	display.clearDisplay();
+	display.setTextColor(WHITE);
+	display.setCursor(0, 0);
+	display.print(menuItem);
+	display.drawFastHLine(0, 10, SCREEN_WIDTH, WHITE);
+	display.setTextSize(2);
+	display.setCursor(0, 15);
+	if (timeItem==0)
+	{
+		display.setTextColor(BLACK, WHITE);
+		display.print(timeToString(time[0]));
+
+		display.setTextColor(WHITE, BLACK);
+		display.print(":");
+		display.print(timeToString(time[1]));
+	}
+	else
+	{
+		display.setTextColor(WHITE, BLACK);
+		display.print(timeToString(time[0]));
+		display.print(":");
+
+		display.setTextColor(BLACK, WHITE);
+		display.print(timeToString(time[1]));
+	}
+
+//	display.print(timeToString(time));
+	display.display();
+}
+
+void displayOnOffControllerMenuPage(String menuItem, uint8_t onValue, uint8_t offValue)
+{
+	display.setTextSize(1);
+	display.clearDisplay();
+	display.setTextColor(WHITE);
+	display.setCursor(0, 0);
+	display.print(menuItem);
+	display.drawFastHLine(0, 10, SCREEN_WIDTH, WHITE);
+	display.setCursor(0, 15);
+	if (subMenuItem==0)
+	{
+		display.setTextColor(BLACK, WHITE);
+		display.print("On:  ");
+		display.println(onValue);
+		
+		display.setTextColor(WHITE, BLACK);
+		display.print("Off: ");
+		display.println(offValue);
+	}
+	else if (subMenuItem==1)
+	{
+		display.print("On:  ");
+		display.println(onValue);
+		
+		display.setTextColor(BLACK, WHITE);
+		display.print("Off: ");
+		display.println(offValue);
+	}
+	display.display();
+}
+
+void displayMainMenuPage()
+{
+	display.setTextSize(1);
+	display.clearDisplay();
+	display.setCursor(0, 0);
+	display.print(timeToString(RTC.now().hour()));
+	display.print(':');
+	display.println((timeToString(RTC.now().minute())));
+	display.display();
+}
+
+const String timeToString(uint8_t time)
+{
+	static char string[2];
+	sprintf(string, "%02d", time);
+	return string;
 }
 
 ISR(PCINT0_vect)
 {
   	cli();
 	if (!digitalRead(SW))
-			swPressed = 1;
+		swPressed = true;
 	else
 	{
 		// Read the current state of CLK
@@ -130,10 +617,16 @@ ISR(PCINT0_vect)
     		// If the DT state is different than the CLK state then
     		// the encoder is rotating CCW so decrement
     		if (digitalRead(DT) != currentStateCLK)
-      			counter --;
-    		else
-				// Encoder is rotating CW so increment
-      			counter ++;
+			{
+				up = true;
+				down = false;
+			}
+    		else	// Encoder is rotating CW so increment
+			{
+				//counter ++;
+				up = false;
+				down = true;
+			}
   		}
 		// Remember last CLK state
 		lastStateCLK = currentStateCLK;
